63025dee6a1cd557f8f2f359876f4a0927e83a49
amandamarker <amandamarker1090@gmail.com>
2025-11-29 11:19:09 -0600

    commit 4

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const wizardsWrapper = document.querySelector('.wizards');
+const search__name = document.querySelector(".search__name")
+
 
 function searchWizards(event) {
     rendorWizards(event.target.value);
+    search__name.innerHTML = event.target.value
 }
 
 async function rendorWizards(searchTerm) {
@@ -12,7 +15,7 @@ async function rendorWizards(searchTerm) {
     console.log(wizardsWrapper);
     wizardsWrapper.innerHTML = wizardsArr.slice(0-6).map((wizard) => {
         return (`
-        <div class="wizards">
+        <div class="wizard">
         <h1>${wizard.firstName || ""} ${wizard.lastName}</h1>
         
         ${wizard.elixirs.map((elixir) => (
@@ -23,7 +26,12 @@ async function rendorWizards(searchTerm) {
 
         </div>
         `);
-    }).join('');
+    }).join('');0
 }
 
 rendorWizards()
+
+function filterWizards(event) {
+        rendorWizards(event.target.value)
+    }
+  
