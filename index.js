d7317e81d7f5cebbc75126044a16c24752e3a9fc
amandamarker <amandamarker1090@gmail.com>
2025-12-01 14:14:14 -0600

    commit 5

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
-const wizardsWrapper = document.querySelector('.wizards');
+const moviesWrapper = document.querySelector('.movies');
 const search__name = document.querySelector(".search__name")
 
-
-function searchWizards(event) {
-    rendorWizards(event.target.value);
+function searchMovies(event) {
+    rendorMovies(event.target.value);
     search__name.innerHTML = event.target.value
 }
 
-async function rendorWizards(searchTerm) {
-    console.log("this is the searchTerm:", searchTerm);  
-    const response = await fetch(
-        'https://wizard-world-api.herokuapp.com/Wizards');
-    const wizardsArr =  await response.json();
-    console.log(wizardsWrapper);
-    wizardsWrapper.innerHTML = wizardsArr.slice(0-6).map((wizard) => {
-        return (`
-        <div class="wizard">
-        <h1>${wizard.firstName || ""} ${wizard.lastName}</h1>
-        
-        ${wizard.elixirs.map((elixir) => (
-        `
-        <p>Elixir: ${elixir.name}</p>
-        `
-        )).join('')}
+let currentMovies = [];
 
-        </div>
-        `);
-    }).join('');0
+async function rendorMovies(searchTerm) { 
+    const response = await fetch(
+        `https://omdbapi.com/?s=${searchTerm}&apikey=c5882ffb`);
+    const data =  await response.json();
+    currentMovies = data.Search;
+    displayMovies(currentMovies);
 }
 
-rendorWizards()
+function displayMovies(movieList) {
+    moviesWrapper.innerHTML = movieList
+    .slice(0, 6)
+    .map((movie) => {
+        return `
+        <div class="movie">
+        <img src="${movie.Poster} alt="" />
+        <h3>${movie.Title}</h3>
+        <p>Year: ${movie.Year}</p>
+    </div>
+        `;
+    }).join('');
+}
 
-function filterWizards(event) {
-        rendorWizards(event.target.value)
+function sortChange(event) {
+    const sortOption = event.target.value;
+    
+    let sortedMovies = [...currentMovies];
+
+    if (sortOption === 'newest') {
+        sortedMovies.sort((a, b) => b.Year - a.Year);
     }
-  
+    else if (sortOption === 'oldest') {
+        sortedMovies.sort((a, b) => a.Year - b.Year);
+    }
+
+    displayMovies(sortedMovies);
+}
+
+function displayMovies(searchTerm) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+           resolve(
+            `https://omdbapi.com/?s=${searchTerm}&apikey=c5882ffb`
+           );
+        }, 1000);
+    })
+}
+
+let movies;
+
+async function rendorMovies();
+    const moviesWrapper = document.querySelector('.movies');
+
+    moviesWrapper.classList += ' movies__loading';
+
+if (!movies) {
+    movies = await getMovies ();
+}
+
+    moviesWrapper.classList.remove('movies__loading');
+
